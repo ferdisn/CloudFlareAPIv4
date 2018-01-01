0ab8bb4464b11a2fa919a2d836baea47b6a7c302
Ferdi <github@ferdi.id>
2018-01-01 09:31:21 +0700

    Add record manipulation.

--- src/id/ferdi/Main.java
+++ src/id/ferdi/Main.java
@@ -5,7 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
         CFApi api = new CFApi();
+
+        //populate zones and records
         api.obtainZones();
+
+        //alter existing record
+        api.findZone("didinofendra.net").findRecord("_acme-challenge.imap.didinofendra.net").updateRecord("tes2");
+
+
+
         System.out.println("");
 
     }
